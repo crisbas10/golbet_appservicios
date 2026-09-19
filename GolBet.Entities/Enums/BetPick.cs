394aa04932a908e5024da351f1189611adb052e1
crisbas10 <crisbaschavarria@gmail.com>
2026-09-18 22:54:37 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Enums/BetPick.cs
@@ -0,0 +1,15 @@
+﻿namespace GolBet.Entities.Enums;
+
+
+
+public enum BetPick
+
+{
+
+    Home = 0,
+
+    Draw = 1,
+
+    Away = 2
+
+}
---
+++ GolBet.Entities/Enums/MatchStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace GolBet.Entities.Enums;
+public enum MatchStatus
+{
+    Scheduled = 0,
+    InProgress = 1,
+    Finished = 2
+}
